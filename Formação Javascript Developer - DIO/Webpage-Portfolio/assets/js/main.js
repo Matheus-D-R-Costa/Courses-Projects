a601a544efe93ca8f246757170ff2c64783de0bb
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-08-22 15:49:00 -0300

    add new Courses

---
+++ Formação Javascript Developer - DIO/Webpage-Portfolio/assets/js/main.js
@@ -0,0 +1,55 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => ` <li><img src="${skill.logo}" alt="${skill.name}"></li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('languages')
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+}
+
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+        <li>
+            <span class="title github">${project.name}</span>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
+        `
+    }).join('')
+}
+
+(async () => {
+    const profileData = await getProfileData()
+    updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
+})()
