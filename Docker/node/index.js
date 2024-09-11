1ceaee40269d63615803e3725ce6a4b8795a6692
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-09-11 15:06:02 -0300

    add new docker studies

--- Docker/node/index.js
+++ Docker/node/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const mysql = require('mysql2')
 
 const app = express()
 const port = 3000
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: '123',
+  database: 'nodedb'
+}
+
+const connection = mysql.createConnection(config)
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
@@ -10,3 +20,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('Rodando na porta ' + port)
 })
+
+const sql = `INSERT INTO peoples(name, age) VALUES ('Matheus', 19)`
+
+connection.query(sql)
+
+connection.end()
