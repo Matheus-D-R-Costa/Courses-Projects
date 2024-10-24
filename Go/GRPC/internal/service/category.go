2851af78cb1d6487c14579dbcaf1e012dc68a90f
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-10-24 16:37:33 -0300

    add new functions to grpc testing project

--- Go/GRPC/internal/service/category.go
+++ Go/GRPC/internal/service/category.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"grpc/internal/database"
 	"grpc/internal/pb"
+	"io"
+
+	"google.golang.org/grpc"
 )
 
 type CategoryService struct {
@@ -17,20 +20,111 @@ func NewCategoryServer(categoryDb database.Category) *CategoryService {
 	}
 }
 
-func (service *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+func (service *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
 	category, err  := service.CategoryDb.Create(in.Name, in.Description)
 	if err != nil {
 		return nil, err
 	}
 
-	categoryResponse := &pb.CategoryResponse{
-		Category: &pb.Category{
+	return &pb.Category{
+		Id: category.Id,
+		Name: category.Name,
+		Description: category.Description,
+	}, nil
+
+}
+
+func (service *CategoryService) CreateCategoyStream(stream grpc.ClientStreamingServer[pb.CreateCategoryRequest, pb.CategoryList]) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+
+		if err != nil {
+			return err
+	  }
+
+		categoryResult, err := service.CategoryDb.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+	  }
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id: categoryResult.Id,
+			Name: categoryResult.Name,
+			Description: category.Description,
+		})
+
+	}
+
+}
+
+func (service *CategoryService) CreateCategoryStreamBidirectional(stream grpc.BidiStreamingServer[pb.CreateCategoryRequest, pb.Category]) error {
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+	  }
+
+		categoryResult, err := service.CategoryDb.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+	  }
+
+		err = stream.Send(&pb.Category{
+			Id: categoryResult.Id,
+			Name: categoryResult.Name,
+			Description: category.Description,
+		})
+
+		if err != nil {
+			return err
+	  }
+
+	}
+}
+
+
+func (service *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := service.CategoryDb.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
 			Id: category.Id,
 			Name: category.Name,
 			Description: category.Description,
-		},
+		}
+
+		categoriesResponse = append(categoriesResponse, categoryResponse)
+
 	}
 
-	return categoryResponse, nil
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
+
+}
+
+func (service *CategoryService) FindCategoryById(ctx context.Context, in *pb.CategoryIdRequest) (*pb.Category, error) {
+	category, err := service.CategoryDb.FindById(in.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Category{
+		Id: category.Id,
+		Name: category.Name,
+		Description: category.Description,
+	}, nil
 
 }
