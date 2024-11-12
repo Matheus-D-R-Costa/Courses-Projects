ec8293d0600ad5c3b0503bcea7dd2dceef58728a
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-11-12 16:30:54 -0300

    add service for product domain

---
+++ typescript/Aluno/src/product/service/product.service.spec.ts
@@ -0,0 +1,22 @@
+import Product from "../product"
+import productService from "./product.service";
+
+describe("Product service unit tests", () => {
+
+  it("should change the prices of all products", () => {
+
+    const product01 = new Product("prd1", "p1", 30);
+    const product02 = new Product("prd2", "p2", 10);
+    const product03 = new Product("prd3", "p3", 5);
+
+    const products = [product01, product02, product03];
+
+    productService.increasePrice(products, 100);
+
+    expect(product01.price).toBe(60);
+    expect(product02.price).toBe(20);
+    expect(product03.price).toBe(10);
+
+  });
+
+});
---
+++ typescript/Aluno/src/product/service/product.service.ts
@@ -0,0 +1,11 @@
+import Product from "../product";
+
+export default class productService {
+
+  static increasePrice(products: Product[], percentage: number): void {
+    products.forEach((product) => {
+      product.changePrice((product.price * percentage) / 100 + product.price);
+    });
+  };
+
+};
