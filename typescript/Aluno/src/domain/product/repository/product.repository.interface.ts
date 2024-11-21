005a8fed00eeb05f01e092d8aa80b444697336b5
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-11-21 16:24:33 -0300

    add new services to orders and create repository interfaces

---
+++ typescript/Aluno/src/domain/product/repository/product.repository.interface.ts
@@ -0,0 +1,6 @@
+import Repository from "../../@shared/repository.interface";
+import Product from "../entity/product";
+
+export default interface ProductRepositoryInterface extends Repository<Product> {
+
+}
